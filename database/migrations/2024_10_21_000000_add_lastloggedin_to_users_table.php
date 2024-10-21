58258358fa37934e25a3553094e67b7bc0874097
George Baboukis <thesupergamer34@gmail.com>
2024-10-21 18:23:42 -0400

    Update 2024_10_21_000000_add_lastloggedin_to_users_table.php

--- database/migrations/2024_10_21_000000_add_lastloggedin_to_users_table.php
+++ database/migrations/2024_10_21_000000_add_lastloggedin_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddLastloggedin extends Migration
+class AddLastLoggedInColumnToUsersTable extends Migration
 {
     /**
      * Run the migrations.
